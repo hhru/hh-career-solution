140a395cfdb8cc44448f55a1c340f68c978865a9
Abdulov Azad <abdulov.azad@yandex.ru>
2021-04-08 13:44:34 +0300

    fix formatting

--- backend/src/main/java/ru/hh/career/solution/Main.java
+++ backend/src/main/java/ru/hh/career/solution/Main.java
@@ -7,6 +7,7 @@ import ru.hh.career.solution.config.ProdConfig;
 import ru.hh.nab.starter.NabApplication;
 
 public class Main {
+
   public static void main(String[] args) {
     NabApplication
         .builder()
