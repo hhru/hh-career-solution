2af80878a07b3b1ef3005a49c172c2be5acf9574
Abdulov Azad <abdulov.azad@yandex.ru>
2021-04-15 12:59:06 +0300

    add account entity

--- backend/init.sql
+++ backend/init.sql
@@ -3,3 +3,10 @@ CREATE TABLE area
   area_id     INTEGER PRIMARY KEY,
   name   VARCHAR(100) NOT NULL
 );
+
+CREATE TABLE account
+(
+  id              serial      primary key,
+  username        text        not null,
+  password_hash   text        not null
+)
---
+++ backend/src/main/java/ru/hh/career/solution/entity/Account.java
@@ -0,0 +1,46 @@
+package ru.hh.career.solution.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "account")
+public class Account {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  private String username;
+
+  @Column(name = "password_hash")
+  private String passwordHash;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPasswordHash() {
+    return passwordHash;
+  }
+
+  public void setPasswordHash(String passwordHash) {
+    this.passwordHash = passwordHash;
+  }
+}
