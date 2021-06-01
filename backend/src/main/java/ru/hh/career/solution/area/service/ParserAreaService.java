4e2e03b00bbce635dcd7ec6f641ab116cd9e4991
BorisVeprintzev <boris.veprintzev@yandex.ru>
2021-06-01 22:09:43 +0300

    correct import

--- backend/src/main/java/ru/hh/career/solution/area/service/ParserAreaService.java
+++ backend/src/main/java/ru/hh/career/solution/area/service/ParserAreaService.java
@@ -9,7 +9,10 @@ import ru.hh.career.solution.dao.GenericDao;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
 @Service
